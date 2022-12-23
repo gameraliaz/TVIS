749bcc3adf5fdcebf7b129d8d00324fab53bf63e
Ali Soleimani <57070726+gameraliaz@users.noreply.github.com>
2022-12-23 17:17:47 +0330

    MainWindow;

--- TVIS/MainWindow.xaml.cs
+++ TVIS/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using TVIS.MVVM.Models;
+using TVIS.MVVM.Views;
 
 namespace TVIS
 {
@@ -7,9 +11,58 @@ namespace TVIS
     /// </summary>
     public partial class MainWindow : Window
     {
+       
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+        void Colapsingmenu(object sender, RoutedEventArgs e)
+        {
+            Storyboard? sb = (Resources["Colapsing"] as Storyboard);
+            sb?.Begin();
+        }
+        void Expandingmenu(object sender, RoutedEventArgs e)
+        {
+            Storyboard? sb = (Resources["Expanding"] as Storyboard);
+            sb?.Begin();
+        }
+
+        private void border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var myWindow = Window.GetWindow(this);
+            myWindow.DragMove();
+        }
+
+        private void Lmenu_MouseDownDashboard(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Lmenu_MouseDownInsertion(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Lmenu_MouseDownModify(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Lmenu_MouseDownDeletetion(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Lmenu_MouseDownInfo(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Lmenu_MouseDownExit(object sender, RoutedEventArgs e)
+        {
+            var myWindow = Window.GetWindow(this);
+            myWindow.Close();
         }
     }
 }
