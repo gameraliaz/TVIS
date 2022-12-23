5da5ffe16c695fdd458fa5d252ef04ae0ae94fe3
Ali Soleimani <57070726+gameraliaz@users.noreply.github.com>
2022-12-23 22:27:36 +0330

    FirstViewModel Added!

---
+++ TVIS/MVVM/ViewModels/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVIS.MVVM.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        protected void OnPropertyChanged(string? propertyname) 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+    }
+}
